a37003ee42db795d702d52f12190490f66c7f19a
Rich <rufhinator@hotmail.com>
2016-02-08 12:43:55 -0500

    Main v0.2
    Most up to date version atm, adds a HUD and timer

---
+++ Assets/My Assets/Scripts/HUD/Timer.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+private var startTime : float;
+var textTime : String;
+
+function Start () {
+
+	startTime = Time.time;
+
+}
+
+function OnGUI () {
+
+	var guiTime = Time.time - startTime;
+
+	var minutes : int = guiTime / 60;
+	var seconds : int = guiTime % 60;
+	var fraction : int = (guiTime * 100) % 100;
+
+	textTime = String.Format ("{0:00} : {1:00} : {2:00}" , minutes, seconds, fraction);
+	GetComponent(UI.Text).text = textTime;
+
+}

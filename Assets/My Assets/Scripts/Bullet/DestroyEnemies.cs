7d1fea5330035263f9ca7aa9a3a7674cf9b14af0
Rich <rufhinator@hotmail.com>
2016-03-06 18:16:34 -0500

    MSAGC v0.3
    have the spawners working

--- Assets/My Assets/Scripts/Bullet/DestroyEnemies.cs
+++ Assets/My Assets/Scripts/Bullet/DestroyEnemies.cs
@@ -5,9 +5,15 @@ public class DestroyEnemies: MonoBehaviour {
 
 	void OnCollisionEnter(Collision col)
 	{
-		if (col.gameObject.name == "Enemy") {
-			Destroy (col.gameObject);
-		}
+        if (col.gameObject.tag == "Enemy")
+        {
+            Destroy(col.gameObject);
+            Destroy(this.gameObject);
+        }
 
+        if (col.gameObject.tag == "Obstacles")
+        {
+            Destroy(this.gameObject);
+        }
 	}
 }

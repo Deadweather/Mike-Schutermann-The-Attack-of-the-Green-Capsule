d7c7b22b12ff1e436466b0d601cde7f4c9b32381
Rich <rufhinator@hotmail.com>
2016-03-19 03:10:50 -0400

    retry
    MSAGC v0.6
    
    Im calling this the v0.6, almost all the gameplay elements
    are in place.
    Changes:
    -New Main menu
    -Further changes to the AI(the
    enemies now have rigidbodies and use
    physics in order to move, this is
    to keep them from popping through
    buildings)
    -Levels now are full of box
    colliders
    -Music has been added to each screen
    -Shooterman
    removed
    (Think thats it!)

---
+++ Assets/My Assets/Scripts/Menu/MainMenuCamera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuCamera : MonoBehaviour {
+
+    private Transform target;
+    private Quaternion xrot;
+
+	// Use this for initialization
+	void Start () {
+
+        GameObject scene = GameObject.Find("streetEnviroment");
+
+        target = scene.GetComponent<Transform>();
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.LookAt(target);
+        transform.Rotate(25,0,0);
+
+        transform.Translate(Vector3.right * 0.5f);
+
+    }
+}
---
+++ Assets/My Assets/Scripts/Menu/MenuMusicCont.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuMusicCont : MonoBehaviour {
+
+    public AudioSource music;
+    public AudioClip currentClip;
+    public AudioClip musicIntro;
+    public AudioClip musicLoop;
+
+    private float musict;
+
+	// Use this for initialization
+	void Start () {
+        music = GetComponent<AudioSource>();
+
+        currentClip = musicIntro;
+
+        music.clip = currentClip;
+        music.Play();
+
+        musict = music.clip.length;
+
+        StartCoroutine(playLoop());
+	}
+
+    IEnumerator playLoop()
+    {
+        yield return new WaitForSeconds(musict);
+
+        currentClip = musicLoop;
+        music.clip = currentClip;
+
+        music.Play();
+
+        musict = music.clip.length;
+
+        StartCoroutine(playLoop());
+    }
+}

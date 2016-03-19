fb9d75dd48eb503f90c354375fdb9e59c069b507
Rich <rufhinator@hotmail.com>
2016-03-19 03:14:09 -0400

    retry

--- Assets/My Assets/Scripts/Enemy Stuff/EnemyMovement.cs
+++ Assets/My Assets/Scripts/Enemy Stuff/EnemyMovement.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class EnemyMovement : MonoBehaviour
 {
 
     private Transform myTransform;
+    private Rigidbody rb;
+    private Vector3 forwardDirection;
     public float speed;
 
     bool follow;
@@ -13,20 +16,38 @@ public class EnemyMovement : MonoBehaviour
 
     void Start()
     {
-
         GameObject player = GameObject.Find("Player");
         target = player.GetComponent<Transform>();
+        rb = this.GetComponent <Rigidbody >();
+
 
         minRange = 1;
-        speed = 0.2f;
-        //target = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);        
+        speed = 1.5f;       
     }
 
 
     void Update()
     {
+
+        forwardDirection = this.transform.TransformDirection(Vector3.forward);
+
+        //transform.Translate(Vector3.forward * speed);
+    }
+
+    void FixedUpdate()
+    {
         transform.LookAt(target);
-        transform.Translate(Vector3.forward * speed);
+        rb.AddForce(forwardDirection * speed);
+    }
+
+    void OnCollisionEnter(Collision C)
+    {
+        if (C.gameObject.tag=="Player")
+        {
+            Destroy(C.gameObject);
+            SceneManager.LoadScene("Lose");
+        }
+       
     }
 
 }
